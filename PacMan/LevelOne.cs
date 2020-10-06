0ba1a59191b3376779bc67b2eabe51424e89ae00
GorginZ <georgia.z.leng@gmail.com>
2020-10-06 11:58:23 +1100

    just comments on wall locatoins

--- PacMan/LevelOne.cs
+++ PacMan/LevelOne.cs
@@ -22,12 +22,13 @@ namespace PacMan
 
     void ILevel.BuildLevel()
     {
+      //top wall
       var wallCoordinates = new List<Coordinates> { new Coordinates(0, 0), new Coordinates(0, 1), new Coordinates(0, 2), new Coordinates(0, 3), new Coordinates(0, 4), new Coordinates(0, 5), new Coordinates(0, 6), new Coordinates(0, 7), new Coordinates(0, 8), new Coordinates(0, 9), new Coordinates(1, 9),
-
+//right edge wall
       new Coordinates(2, 9), new Coordinates(3, 9), new Coordinates(4, 9), new Coordinates(6, 9), new Coordinates(7, 9), new Coordinates(8, 9), new Coordinates(9, 9), new Coordinates(10, 10), new Coordinates(10, 9),
-
+//left edge wall
       new Coordinates(1, 0), new Coordinates(2, 0), new Coordinates(3, 0), new Coordinates(4, 0), new Coordinates(6, 0), new Coordinates(7, 0), new Coordinates(8, 0), new Coordinates(9, 0),
-
+//bottom wall
       new Coordinates(10, 0), new Coordinates(10, 1), new Coordinates(10, 2), new Coordinates(10, 3), new Coordinates(10, 4), new Coordinates(10, 5), new Coordinates(10, 6), new Coordinates(10, 7), new Coordinates(10, 8),
 //row one
       new Coordinates(2, 2),new Coordinates(2, 3),new Coordinates(2, 4),
